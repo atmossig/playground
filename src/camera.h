22c80df5d66d480cd2c10df4e3e1651d85566a01
grant <abernathy.grant@icloud.com>
2025-07-04 20:24:59 -0500

    Camera header

--- src/camera.h
+++ src/camera.h
@@ -1,1 +1,32 @@
-#pragma once
+#ifndef CAMERA_H
+#define CAMERA_H
+
+class Camera {
+public:
+	Camera();
+
+	void UpdateView();
+	void UpdateProjection(int width, int height);
+	void ProcessMouseMovement(float xoffset, float yoffset);
+
+	// Movement methods
+	void MoveForward(float deltaTime);
+	void MoveBackward(float deltaTime);
+	void MoveLeft(float deltaTime);
+	void MoveRight(float deltaTime);
+private:
+	void UpdateCameraVectors();
+
+	// Camera attributes
+	float position[3];
+	float front[3];
+	float up[3];
+	float right[3];
+	const float worldUp[3] = { 0.0f, 1.0f, 0.0f };
+
+	// Euler angles
+	float yaw;
+	float pitch;
+};
+
+#endif // CAMERA_H
