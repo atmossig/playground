9dc7d1b5a956725e56ea6d510f6f42f7336de1c3
grant <abernathy.grant@icloud.com>
2025-07-04 20:23:26 -0500

    Config file

--- src/config.h
+++ src/config.h
@@ -1,1 +1,15 @@
-#pragma once
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Window settings
+constexpr int SCREEN_WIDTH = 1280;
+constexpr int SCREEN_HEIGHT = 720;
+
+// Camera settings
+constexpr float CAMERA_FOV = 75.0f;				// FOV degres
+constexpr float CAMERA_NEAR_PLANE = 0.1f;		// Near plane distance
+constexpr float CAMERA_FAR_PLANE = 100.0f;		// Far plane distance
+constexpr float	CAMERA_SPEED = 5.0f;			// In units per second
+constexpr float MOUSE_SENSITIVITY = 0.1f;		// Sensitivity for mouse movement
+
+#endif // CONFIG_H
